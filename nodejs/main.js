89bb5029219bfc71b352828e58833cf2a1ba6d2f
juicyorange <rlaxodud980909@naver.com>
2020-08-25 22:41:46 +0900

    #생활코딩 Node.js (App - 파일을 이용해 본문구현 ~  App - 함수를 이용해서 정리정돈하기)

--- nodejs/main.js
+++ nodejs/main.js
@@ -3,51 +3,84 @@ var fs = require('fs');
 var url = require('url');
 // var 변수명 = require('모듈') 변수명이 해당 모듈을 사용할 것이라고 하는것.
 
+function templateHTML(_title, _list, _body){
+  var template = `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${_title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${_list}
+    ${_body}
+  </body>
+  </html>
+  `;
+  return template;
+}
+
+function templateList(_filstlist){
+  /*
+  var list = `<ul>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ul>`
+  아래에 위 코드를 반복문 형식으로 변한 -> 훨씬 효율적임!!
+  */
+  var list = '<ul>';
+  var idx = 0;
+  while(idx<_filstlist.length){
+    list = list+`<li><a href="/?id=${_filstlist[idx]}">${_filstlist[idx]}</a></li>`;
+    idx++;
+  }
+  list = list+'</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     //console.log(_url); //query string 전체가 받아짐을 알 수 있다.
     var queryData = url.parse(_url, true).query;
     //queryData에는 querystring의 정보들이 객체형식으로 담겨져있다.
     //console.log(queryData.id); //queryData가 객체형식으로 되어있으므로 .id를 하면 id 값이 받아짐을 알 수 있다.
-    var title = queryData.id;
+    var pathname = url.parse(_url, true).pathname;
+    //console.log(url.parse(_url, true)); URL 정보표시
 
-    if(_url == '/'){
-      _url = '/index.html';
-      title = 'welcome';
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        //.id가 아무것도 없다면 여기로 옴. undefined = 정의안되어있다는 뜻
+        fs.readdir('./data', function(error, filelist){
+          //폴더에있는 파일들을 filelist변수에 배열형식으로 저장함.
+          //console.log(filelist); filstlist 출력해보기
+
+          var title = 'Welcome';
+          var description = 'Hello, NodeJS';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`) ;
+          //console.log(__dirname + _url); url 나오는 것 확인하는 코드
+          response.writeHead(200); //200이 의미하는 것은 파일을 성공적으로 전송했다는 것을 의미
+          response.end(template);
+          //response.end(fs.readFileSync(__dirname + _url));
+        })
+      }
+      else{
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          fs.readdir('./data', function(error, filelist){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`) ;
+            response.writeHead(200); //200이 의미하는 것은 파일을 성공적으로 전송했다는 것을 의미
+            response.end(template);
+          });
+        });
+      }
     }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    response.writeHead(200);
-    var template =  `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-      <img src="coding.jpg" width="100%">
-      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-      </p>
-    </body>
-    </html>
-    `;
-
-    console.log(__dirname + _url);
-    response.end((template));
-    //response.end(fs.readFileSync(__dirname + _url));
-
-
-});
+    else{
+      response.writeHead(404); //404가 의미하는 것은 올바르지 않다는 것을 의미
+      response.end("Not Found 404");
+      }
+    });
 app.listen(3000);
