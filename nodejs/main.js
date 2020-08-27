c10c2062e6847219963cc76ead49197897163fe1
juicyorange <rlaxodud980909@naver.com>
2020-08-28 00:54:08 +0900

    Add: #opentutorials node.js 글생성 UI만들기 ~ 파일생성과 리다이렉션

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 // var 변수명 = require('모듈') 변수명이 해당 모듈을 사용할 것이라고 하는것.
 
 function templateHTML(_title, _list, _body){
@@ -14,6 +15,7 @@ function templateHTML(_title, _list, _body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${_list}
+    <a href = "/create">create</a>
     ${_body}
   </body>
   </html>
@@ -47,7 +49,7 @@ var app = http.createServer(function(request,response){
     //queryData에는 querystring의 정보들이 객체형식으로 담겨져있다.
     //console.log(queryData.id); //queryData가 객체형식으로 되어있으므로 .id를 하면 id 값이 받아짐을 알 수 있다.
     var pathname = url.parse(_url, true).pathname;
-    //console.log(url.parse(_url, true)); URL 정보표시
+    //console.log(url.parse(_url, true)); //URL 정보표시
 
     if(pathname === '/'){
       if(queryData.id === undefined){
@@ -77,6 +79,53 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+    }
+    else if(pathname==='/create'){
+      fs.readdir('./data', function(error, filelist){
+        //폴더에있는 파일들을 filelist변수에 배열형식으로 저장함.
+        //console.log(filelist); filstlist 출력해보기
+
+        var title = 'Create_HTML';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2>
+          <form action="http://localhost:3000/proccess_create" method="post">
+            <p><input type = "text" name = "title" placeholder="title"></p>
+            <p>
+              <textarea name="description" rows="8" cols="80" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type = "submit">
+            </p>
+          </form>
+          `) ;
+        //console.log(__dirname + _url); url 나오는 것 확인하는 코드
+        response.writeHead(200); //200이 의미하는 것은 파일을 성공적으로 전송했다는 것을 의미
+        response.end(template);
+        //response.end(fs.readFileSync(__dirname + _url));
+      });
+    }
+    else if(pathname ==='/proccess_create'){
+      var body = '';
+      request.on('data', function(data){
+        //데이터를 받아오는데 조각조각 받아옴.
+        //조각조각 받아서 콜백함수를 그때마다 호출함.
+        body = body + data;
+        //그렇기때문에 body = data가 아닌 body에 data를 계속 더하는 방식으로 구현.
+        /*if(body.length >1e6){
+          request.connection.destroy();
+          //너무 많은 정보가 들어오면 request를 파괴함.
+        }*/
+      });
+      request.on('end', function(){
+        //데이터가 조각조각 들어오다 더 들어올 데이터가 없으면 콜백함수를 호출. 즉, 정보수신이 끝났다는 뜻
+        var post = qs.parse(body);//데이터 객체로 넘어옴
+        var description = post.description;
+        var title = post.title;
+        fs.writeFile(`./data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302 , {Location: `/?id=${title}`});
+          response.end();
+        });
+      });
     }
     else{
       response.writeHead(404); //404가 의미하는 것은 올바르지 않다는 것을 의미
